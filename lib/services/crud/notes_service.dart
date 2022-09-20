c9de80d74bcd696570efbe6407e26a1b27f7a12d
Malcolm Cumming <malcolmsgc@gmail.com>
2022-09-20 12:26:40 +0100

    DatabaseNote class

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -4,6 +4,12 @@ import 'package:path_provider/path_provider.dart'
     show getApplicationDocumentsDirectory;
 import 'package:path/path.dart' show join;
 
+class NotesService {
+  Database? _db;
+
+  Future<void> open() async {}
+}
+
 @immutable
 class DatabaseUser {
   final int id;
@@ -27,5 +33,42 @@ class DatabaseUser {
   int get hashCode => id.hashCode;
 }
 
+class DatabaseNote {
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+
+  DatabaseNote({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.isSyncedWithCloud,
+  });
+
+  DatabaseNote.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud =
+            (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
+
+  @override
+  String toString() =>
+      'Note, ID = $id, userId = $userId,  isSyncedWithCloud = $isSyncedWithCloud';
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+const dbName = 'notes.db';
+const noteTable = 'note';
+const userTable = 'user';
 const idColumn = 'id';
 const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithCloudColumn = 'is_synced_with_cloud';
