ed0d2a87cff2bba2bcecff4249307bd4ad373335
Malcolm Cumming <malcolmsgc@gmail.com>
2022-09-27 12:21:26 +0100

    add divider to notes tile

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -15,31 +15,46 @@ class NotesListView extends StatelessWidget {
     required this.onDeleteNote,
   }) : super(key: key);
 
+  Widget dividedTile(BuildContext context, Widget tile, Color color) {
+    return DecoratedBox(
+      position: DecorationPosition.foreground,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: Divider.createBorderSide(context, color: color),
+        ),
+      ),
+      child: tile,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: notes.length,
         itemBuilder: ((context, index) {
           final note = notes[index];
-          return ListTile(
-            title: Text(
-              note.text,
-              maxLines: 1,
-              softWrap: true,
-              overflow: TextOverflow.fade,
-            ),
-            trailing: IconButton(
-              onPressed: () async {
-                final shouldDelete = await showDeleteDialog(context);
-                if (shouldDelete) {
-                  onDeleteNote(note);
-                }
-              },
-              icon: Icon(Icons.delete),
-              color: Colors.pink,
-            ),
-            tileColor: Color.fromARGB(255, 247, 223, 242),
-          );
+          return dividedTile(
+              context,
+              ListTile(
+                title: Text(
+                  note.text,
+                  maxLines: 1,
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+                ),
+                trailing: IconButton(
+                  onPressed: () async {
+                    final shouldDelete = await showDeleteDialog(context);
+                    if (shouldDelete) {
+                      onDeleteNote(note);
+                    }
+                  },
+                  icon: Icon(Icons.delete),
+                  color: Colors.pink,
+                ),
+                tileColor: Color.fromARGB(255, 247, 223, 242),
+              ),
+              Colors.white);
         }));
   }
 }
